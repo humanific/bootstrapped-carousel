0b293ed9433172ce5af4cbe15774f5c14bb61017
humanific <francois@humanific.com>
2014-12-15 14:39:02 +0700

    v0.01

--- index.php
+++ index.php
@@ -1,82 +1,69 @@
 <?php
 /**
- * @package Hello_Dolly
- * @version 1.6
+ * @package bootstrapped_carousel
+ * @version 0.1
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.6
-Author URI: http://ma.tt/
+Plugin Name: Bootstrapped carousel
+Plugin URI: https://github.com/humanific/bootstrapped-carousel
+Description: Shortcode for displaying bootstrap carousels
+Author: Francois Richir
+Version: 0.1
+Author URI: http://humanific.com
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
-
-	// Here we split it into lines
-	$lyrics = explode( "\n", $lyrics );
-
-	// And then randomly choose a line
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+function bootstrapped_carousel_get($images){
+	global $carouselid;
+	$carouselid++;
+	ob_start();
+	if ($images) :?>
+		<div class="carousel slide" data-ride="carousel" id="carousel<?php echo $carouselid ?>">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+			<?php 
+			foreach( $images as $k => $imagePost ): ?>
+				<li data-target="#carousel<?php echo $carouselid ?>" data-slide-to="<?php echo $k;?>" <?php if($k==0) : ?> class="active" <?php endif; ?>></li>
+			<?php endforeach ;?>
+		  </ol>
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+			<?php 
+			foreach( $images as $k => $imagePost ): 
+				$image_attributes = wp_get_attachment_image_src(  $imagePost->ID, 'big' ); 
+				?>
+				<div class="item<?php if($k==0) : ?> active<?php endif; ?>" ><img src="<?php echo $image_attributes[0]; ?>" /></div>
+			<?php endforeach ;?>
+		  </div>
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel<?php echo $carouselid ?>" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel<?php echo $carouselid ?>" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+		  </a>
+		</div>
+		<?php endif; 
+		return ob_get_clean();
 }
 
-// This just echoes the chosen line, we'll position it later
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>$chosen</p>";
+
+function bootstrapped_carousel_shortcode( $atts, $content = null ) {
+   global $post;
+   if(isset($atts['ids'])){
+   		$pids = explode(',', $atts['ids']);
+	   $ids = array();
+	   foreach( $pids as $id ) $ids[] = intval($id);
+   		$wpq = new WP_Query( array(  'post__in' => $ids ,'post_type' => 'attachment','post_status'=>'inherit') );
+		$images = $wpq->posts;
+   }else if(isset($atts['postid'])){
+		$images =get_children( array('post_parent' => $atts['postid'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID') );
+   }else{
+   		$images =get_children( array('post_parent' => $post->ID, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => 'ASC', 'orderby' => 'menu_order ID') );
+   }
+   return bootstrapped_carousel_get($images);
 }
 
-// Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hello_dolly' );
+add_shortcode( 'carousel', 'bootstrapped_carousel_shortcode' );
 
-// We need some CSS to position the paragraph
-function dolly_css() {
-	// This makes sure that the positioning is also good for right-to-left languages
-	$x = is_rtl() ? 'left' : 'right';
-
-	echo "
-	<style type='text/css'>
-	#dolly {
-		float: $x;
-		padding-$x: 15px;
-		padding-top: 5px;		
-		margin: 0;
-		font-size: 11px;
-	}
-	</style>
-	";
-}
-
-add_action( 'admin_head', 'dolly_css' );
 
 ?>
